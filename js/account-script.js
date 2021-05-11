b77b6eff624e34a733fe002f91764328f1499257
WhiteQua <jyh815@gmail.com>
2021-05-11 10:45:48 +0900

    Add files via upload

--- js/account-script.js
+++ js/account-script.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
     $('#m-account').click(function(){
         $('#m-account-num').toggle('slow');
     });
+
+    var clipboard = new Clipboard('#m-account');
+    clipboard.on('success', function(e) {
+        console.log(e);
+    });
+
+    clipboard.on('error', function(e) {
+        console.log(e);
+    });
+
+    var wAccClipboard = new Clipboard('#w-account');
+    wAccClipboard.on('success', function(e) {
+        console.log(e);
+    });
+
+    wAccClipboard.on('error', function(e) {
+        console.log(e);
+    });
 });
